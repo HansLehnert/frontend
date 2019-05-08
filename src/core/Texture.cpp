2a7bf4e94d00d5dd6c985fc326f66f720c625ec7
Hans Lehnert <hans.lehnert@gmail.com>
2019-05-08 11:14:36 -0400

    Textures are now created by factory funcitons

--- src/core/Texture.cpp
+++ src/core/Texture.cpp
@@ -1,4 +1,4 @@
-#include "core/Texture.h"
+#include "core/Texture.hpp"
 
 #include <iostream>
 #include <cstring>
@@ -7,129 +7,94 @@
 #include <SDL2/SDL_surface.h>
 
 
-Texture::Texture() : loaded(false) {
-	glGenTextures(1, &id);
-
-	return;
-}
-
-
-Texture::Texture(const Texture& texture) {
-	*this = texture;
-}
-
-
-Texture::Texture(Texture&& texture) {
-	id = texture.id;
-	width = texture.width;
-	height = texture.height;
-	image_buffer = texture.image_buffer;
+Texture::Texture() {
+    glGenTextures(1, &id);
 }
 
 
 Texture::~Texture() {
-	glDeleteTextures(1, &id);
-
-	if (loaded)
-		delete image_buffer;
-	//SOIL_free_image_data(image_buffer);
-	return;
+    glDeleteTextures(1, &id);
 }
 
 
-Texture& Texture::operator=(const Texture& a) {
-	loadData(a.image_buffer, a.width, a.height);
+std::shared_ptr<Texture> Texture::fromFile(
+        std::string filename, GLuint filter) {
+    std::shared_ptr<Texture> texture = std::make_shared<Texture>();
 
-	return *this;
+    SDL_Surface* surface = IMG_Load(filename.c_str());
+
+    if (surface != nullptr) {
+        //convert to RGBA
+        SDL_Surface* surface_rgba;
+        surface_rgba = SDL_ConvertSurfaceFormat(
+            surface, SDL_PIXELFORMAT_ABGR8888, 0);
+
+        texture->width = surface->w;
+        texture->height = surface->h;
+        texture->channels = 4;
+        texture->bufferData((unsigned char*)surface_rgba->pixels, filter);
+
+        SDL_FreeSurface(surface_rgba);
+        SDL_FreeSurface(surface);
+    }
+    else {
+        std::cout << "[Texture]\tFailed to load " << filename << std::endl;
+    }
+
+    return texture;
 }
 
 
-int Texture::loadFile(std::string filename, GLuint filter) {
-	SDL_Surface* surface = IMG_Load(filename.c_str());
+std::shared_ptr<Texture> Texture::fromData(
+        const unsigned char* data,
+        unsigned int width,
+        unsigned int height,
+        unsigned int channels,
+        GLuint filter
+) {
+    std::shared_ptr<Texture> texture = std::make_shared<Texture>();
 
-	if (surface != nullptr) {
-		//convert to RGBA
-		SDL_Surface* surface_rgba;
-		surface_rgba = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_ABGR8888, 0);
+    texture->width = width;
+    texture->height = height;
+    texture->channels = channels;
+    texture->bufferData(data, filter);
 
-		loadData((unsigned char*)surface_rgba->pixels, surface->w, surface->h, 4, filter);
-
-		SDL_FreeSurface(surface_rgba);
-		SDL_FreeSurface(surface);
-		loaded = true;
-		return 1;
-	}
-	else {
-		std::cout << "[Texture]\tFailed to load " << filename << std::endl;
-		loaded = false;
-		return 0;
-	}
-
-	/*image_buffer = SOIL_load_image(filename.c_str(), &width, &height, NULL, SOIL_LOAD_RGBA);
-
-	if (image_buffer) {
-		glBindTexture(GL_TEXTURE_2D, id);
-
-		bufferTexture(filter);
-
-		return 1;
-	}
-	else {
-		std::cout << "[Texture]\tFailed to load " << filename << std::endl;
-		return 0;
-	}*/
-
-
+    return texture;
 }
 
 
-void Texture::loadData(const unsigned char* data, int w, int h, int c, GLuint filter) {
-	if (loaded)
-		delete image_buffer;
+void Texture::bufferData(const unsigned char* data, GLuint filter) {
+    glBindTexture(GL_TEXTURE_2D, id);
 
-	image_buffer = new unsigned char[w * h * c];
-	memcpy(image_buffer, data, w * h * c);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
 
-	width = w;
-	height = h;
-
-	bufferTexture(filter);
-
-	loaded = true;
-}
-
-
-bool Texture::isLoaded() {
-	return loaded;
-}
-
-
-void Texture::bufferTexture(GLuint filter) {
-	glBindTexture(GL_TEXTURE_2D, id);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_buffer);
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RGBA,
+        width,
+        height,
+        0,
+        GL_RGBA,
+        GL_UNSIGNED_BYTE,
+        data
+    );
 }
 
 
 GLuint Texture::getId() {
-	return id;
+    return id;
 }
 
 
 int Texture::getWidth() {
-	return width;
+    return width;
 }
 
 
 int Texture::getHeight() {
-	return height;
-}
-
-const unsigned char* Texture::getData() {
-	return image_buffer;
+    return height;
 }
---
+++ src/core/Texture.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include "gl_inc.h"
+
+class Texture {
+public:
+    Texture();
+    ~Texture();
+
+    /**
+     * @brief Load texture from raw data.
+     */
+    static std::shared_ptr<Texture> fromData(
+        const unsigned char* data,
+        unsigned int width,
+        unsigned int height,
+        unsigned int channels = 4,
+        GLuint filter = GL_LINEAR
+    );
+
+    /**
+     * @brief Read texture data from file.
+     */
+    static std::shared_ptr<Texture> fromFile(
+        std::string filename,
+        GLuint filter = GL_LINEAR
+    );
+
+
+    int getWidth();
+    int getHeight();
+    GLuint getId();
+
+private:
+    GLuint id;
+
+    unsigned int width;
+    unsigned int height;
+    unsigned int channels;
+
+    void bufferData(const unsigned char* data, GLuint filter);
+};
