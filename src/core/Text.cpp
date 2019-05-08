f18fbdac6e1559483c476aa183a865d565a5ae68
Hans Lehnert <hans.lehnert@gmail.com>
2019-05-08 11:15:42 -0400

    Text is no longer a class

--- src/core/Text.cpp
+++ src/core/Text.cpp
@@ -1,4 +1,4 @@
-#include "Text.h"
+#include "Text.hpp"
 
 #include <string>
 #include <iostream>
@@ -6,168 +6,130 @@
 #include FT_GLYPH_H
 
 #include "glm/glm.hpp"
-#include "core/Texture.h"
-#include "core/Program.h"
+#include "core/Texture.hpp"
+#include "core/Program.hpp"
 
 
-FT_Library Text::ft_library;
-FT_Face Text::ft_face;
+FT_Library ft_library;
+FT_Face ft_face;
 
 
-Text::Text(std::string text) {
-	setText(text);
-};
+bool initFreetype() {
+    FT_Error ft_error;
 
-
-
-bool Text::initFreetype() {
-	FT_Error ft_error;
-
-	ft_error = FT_Init_FreeType(&ft_library);
-	if (ft_error) {
-		std::cout << "[Text]\tFailed to initialize Freetype library" << std::endl;
-		return false;
-	}
-	else {
-		return true;
-	}
+    ft_error = FT_Init_FreeType(&ft_library);
+    if (ft_error) {
+        std::cout
+            << "[Text]\tFailed to initialize Freetype library" << std::endl;
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 
-bool Text::loadTypeface(std::string font_path) {
-	FT_Error ft_error;
+bool loadTypeface(std::string font_name) {
+    FT_Error ft_error;
 
-	ft_error = FT_New_Face(ft_library, font_path.c_str(), 0, &ft_face);
-	if (ft_error) {
-		std::cout << "[Text]\tFailed to load " << font_path << "font." << std::endl;
-		return false;
-	}
-	else {
-		FT_Set_Pixel_Sizes(ft_face, 0, 64);
-		return true;
-	}
+    ft_error = FT_New_Face(ft_library, font_name.c_str(), 0, &ft_face);
+    if (ft_error) {
+        std::cout
+            << "[Text]\tFailed to load " << font_name << " font. "
+            << "Error " << ft_error << "."
+            << std::endl;
+        return false;
+    }
+    else {
+        FT_Set_Pixel_Sizes(ft_face, 0, 64);
+        return true;
+    }
 }
 
 
-void Text::setText(std::string new_text) {
-	text = new_text;
-	updateTexture();
+std::shared_ptr<Texture> renderText(std::string text, TextFormat format) {
+    FT_Error ft_error;
 
-	/*int char_width = font.getWidth() / 16;
-	int char_height = font.getHeight() / 16;
-	int width = (char_width + 1) * text.length() - 1;
-	int height = char_height;
+    FT_Glyph* glyphs = new FT_Glyph[text.length()];
+    int* glyph_pos = new int[text.length()];
 
-	const unsigned char* font_data = font.getData();
-	unsigned char* image_data = new unsigned char[width * height * 4];
+    unsigned int glyph_index;
+    unsigned int previous_index = 0;
 
-	for (int i = 0; i < width * height * 4; i++)
-		image_data[i] = 0;
+    unsigned int width = 0;
+    unsigned int height = ft_face->size->metrics.height >> 6;
+    int offset_x;
+    int offset_y = ft_face->size->metrics.ascender >> 6;
 
-	for (unsigned int i = 0; i < text.length(); i++) {
-		for (int x = 0; x < char_width; x++) {
-			for (int y = 0; y < char_height; y++) {
-				int offset_image = (i * (char_width + 1) + x + y * width) * 4;
-				int offset_font = ((text[i] % 16) * char_width + x + ((text[i] / 16) * char_height + y) * font.getWidth()) * 4;
+    FT_Vector pen;
+    pen.x = 0;
 
-				//Copy the 4 channels
-				for (int c = 0; c < 4; c++) {
-					image_data[offset_image + c] = font_data[offset_font + c];
-				}
-			}
-		}
-	}
+    for (unsigned int n = 0; n < text.length(); n++) {
+        glyph_index = FT_Get_Char_Index(ft_face, text[n]);
 
-	texture.loadData(image_data, width, height, 4, GL_NEAREST);
-	setScale(glm::vec3(width, height, 0) * 0.01f);
-	setPosition(glm::vec3(0));
+        FT_Vector kerning;
+        FT_Get_Kerning(ft_face, previous_index, glyph_index, FT_KERNING_DEFAULT, &kerning);
 
-	delete image_data;*/
-}
+        ft_error = FT_Load_Glyph(ft_face, glyph_index, FT_LOAD_DEFAULT);
+        if (ft_error)
+            continue;
 
+        ft_error = FT_Get_Glyph(ft_face->glyph, &glyphs[n]);
+        if (ft_error)
+            continue;
+        FT_BBox bounding_box;
+        FT_Glyph_Get_CBox(glyphs[n], FT_GLYPH_BBOX_PIXELS, &bounding_box);
 
-void Text::updateTexture() {
-	FT_Error ft_error;
+        if (n == 0) {
+            offset_x = -bounding_box.xMin;
+            width += -bounding_box.xMin;
+        }
 
-	FT_Glyph* glyphs = new FT_Glyph[text.length()];
-	int* glyph_pos = new int[text.length()];
+        if (n == text.length() - 1) {
+            width += bounding_box.xMax + 1;
+        }
 
-	unsigned int glyph_index;
-	unsigned int previous_index = 0;
+        width += (ft_face->glyph->advance.x >> 6) + (kerning.x >> 6);
 
-	unsigned int width = 0;
-	unsigned int height = ft_face->size->metrics.height >> 6;
-	int offset_x;
-	int offset_y = ft_face->size->metrics.ascender >> 6;
+        pen.x += -kerning.x >> 6;
+        glyph_pos[n] = pen.x;
+        pen.x += ft_face->glyph->advance.x >> 6;
 
-	FT_Vector pen;
-	pen.x = 0;
+        previous_index = glyph_index;
+    }
 
-	for (unsigned int n = 0; n < text.length(); n++) {
-		glyph_index = FT_Get_Char_Index(ft_face, text[n]);
+    unsigned char* bitmap = new unsigned char[width * height * 4];
+    for (unsigned int i = 0; i < width * height; i++) {
+        bitmap[i *  4] = 255;
+        bitmap[i *  4 + 1] = 255;
+        bitmap[i *  4 + 2] = 255;
+        bitmap[i *  4 + 3] = 0;
+    }
 
-		FT_Vector kerning;
-		FT_Get_Kerning(ft_face, previous_index, glyph_index, FT_KERNING_DEFAULT, &kerning);
+    for (unsigned int n = 0; n < text.length(); n++) {
+        pen.x = offset_x + glyph_pos[n];
+        pen.y = offset_y;
 
-		ft_error = FT_Load_Glyph(ft_face, glyph_index, FT_LOAD_DEFAULT);
-		if (ft_error)
-			continue;
+        FT_Glyph_To_Bitmap(&glyphs[n], FT_RENDER_MODE_NORMAL, &pen, 0);
+        FT_BitmapGlyph bit = (FT_BitmapGlyph)glyphs[n];
 
-		ft_error = FT_Get_Glyph(ft_face->glyph, &glyphs[n]);
-		if (ft_error)
-			continue;
-		FT_BBox bounding_box;
-		FT_Glyph_Get_CBox(glyphs[n], FT_GLYPH_BBOX_PIXELS, &bounding_box);
+        for (unsigned int i = 0; i < bit->bitmap.width; i++) {
+            for (unsigned int j = 0; j < bit->bitmap.rows; j++) {
+                unsigned int pixel_offset = (i + pen.x + bit->left + width * (j + pen.y - bit->top)) * 4 + 3;
+                if (pixel_offset < width * height * 4)
+                    bitmap[pixel_offset] = bit->bitmap.buffer[i + bit->bitmap.pitch * j];
+            }
+        }
 
-		if (n == 0) {
-			offset_x = -bounding_box.xMin;
-			width += -bounding_box.xMin;
-		}
+        FT_Done_Glyph(glyphs[n]);
+    }
 
-		if (n == text.length() - 1) {
-			width += bounding_box.xMax + 1;
-		}
-		
-		width += (ft_face->glyph->advance.x >> 6) + (kerning.x >> 6);
+    std::shared_ptr<Texture> texture = Texture::fromData(
+        bitmap, width, height, 4, GL_LINEAR);
 
-		pen.x += -kerning.x >> 6;
-		glyph_pos[n] = pen.x;
-		pen.x += ft_face->glyph->advance.x >> 6;
+    delete[] bitmap;
+    delete[] glyphs;
+    delete[] glyph_pos;
 
-		previous_index = glyph_index;
-	}
-
-	unsigned char* bitmap = new unsigned char[width * height * 4];
-	for (unsigned int i = 0; i < width * height; i++) {
-		bitmap[i *  4] = 255;
-		bitmap[i *  4 + 1] = 255;
-		bitmap[i *  4 + 2] = 255;
-		bitmap[i *  4 + 3] = 0;
-	}
-
-	for (unsigned int n = 0; n < text.length(); n++) {
-		pen.x = offset_x + glyph_pos[n];
-		pen.y = offset_y;
-
-		FT_Glyph_To_Bitmap(&glyphs[n], FT_RENDER_MODE_NORMAL, &pen, 0);
-		FT_BitmapGlyph bit = (FT_BitmapGlyph)glyphs[n];
-
-		for (unsigned int i = 0; i < bit->bitmap.width; i++) {
-			for (unsigned int j = 0; j < bit->bitmap.rows; j++) {
-				unsigned int pixel_offset = (i + pen.x + bit->left + width * (j + pen.y - bit->top)) * 4 + 3;
-				if (pixel_offset < width * height * 4)
-					bitmap[pixel_offset] = bit->bitmap.buffer[i + bit->bitmap.pitch * j];
-			}
-		}
-
-		FT_Done_Glyph(glyphs[n]);
-	}
-
-	loadData(bitmap, width, height, 4, GL_LINEAR);
-	//setScale(glm::vec3(width, height, 0) * 0.001f);
-	//setPosition(glm::vec3(0));
-
-	delete bitmap;
-	delete glyphs;
-	delete glyph_pos;
+    return texture;
 }
---
+++ src/core/Text.hpp
@@ -0,0 +1,30 @@
+/*
+ * Text related functions
+ *
+ * Functions for loading fonts and generating text textures.
+ */
+
+#pragma once
+
+#include <string>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "core/Texture.hpp"
+
+
+// Text style settings for rendering text
+struct TextFormat {
+    int font_size;
+};
+
+
+// Initialize the freetype library
+bool initFreetype();
+
+// Load a typeface
+bool loadTypeface(std::string font_name);
+
+// Renders text into a Texture
+std::shared_ptr<Texture> renderText(std::string text, TextFormat format);
